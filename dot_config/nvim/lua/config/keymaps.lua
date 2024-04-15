fbd56bc308201d22e6ee1abb53730277d2eb4883
Mika Andrianarijaona <mika.andrianarijaona@busbud.com>
2024-04-15 10:15:46 +0200

    feat: add chezmoi to telescope

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -3,15 +3,17 @@
 -- Add any additional keymaps here
 
 local function map(mode, lhs, rhs, opts)
-  local keys = require("lazy.core.handler").handlers.keys
-  ---@cast keys LazyKeysHandler
-  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
-    opts = opts or {}
-    opts.silent = opts.silent ~= false
-    vim.keymap.set(mode, lhs, rhs, opts)
-  end
+	local keys = require("lazy.core.handler").handlers.keys
+	---@cast keys LazyKeysHandler
+	if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+		opts = opts or {}
+		opts.silent = opts.silent ~= false
+		vim.keymap.set(mode, lhs, rhs, opts)
+	end
 end
 
 map("n", "<leader>fy", "<cmd>let @*=expand('%')<cr>", { desc = "Yank file path to clipboard" })
 map("n", "<leader>fY", "<cmd>echo expand('%')<cr>", { desc = "Echo file path" })
 map("n", "<leader>ce", "<cmd>EslintFixAll<cr>", { desc = "EslintFixAll" })
+local telescope = require("telescope")
+map("n", "<leader>cz", telescope.extensions.chezmoi.find_files, { desc = "Chezmoi" })
