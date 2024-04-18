11e5068a42c5638aa45009d6323e5fcba3da9425
Mika Andrianarijaona <mika.andrianarijaona@busbud.com>
2024-04-18 11:20:35 +0200

    feat: add empty mocharc config to override existing

---
+++ dot_empty-mocharc.js
@@ -0,0 +1,4 @@
+// This can be use to override existing mocharc.js file
+// in neottest configurations like so : 
+// vim.g.neotest_mocha_command = "npx mocha -r 'ts-node/register/transpile-only' --config /Users/mikaoelitiana/.empty-mocharc.js
+module.exports = {};
