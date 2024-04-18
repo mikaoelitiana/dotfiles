016a5892a85e5bb049ff0467de6d14fc65581850
Mika Andrianarijaona <mika.andrianarijaona@busbud.com>
2024-04-18 12:35:31 +0200

    fix: update mocha config

--- dot_empty-mocharc.js
+++ dot_empty-mocharc.js
@@ -1,4 +1,7 @@
 // This can be use to override existing mocharc.js file
 // in neottest configurations like so : 
 // vim.g.neotest_mocha_command = "npx mocha -r 'ts-node/register/transpile-only' --config /Users/mikaoelitiana/.empty-mocharc.js
-module.exports = {};
+module.exports = {
+  recursive: true,
+  extension: ["ts", "js", "tsx"]
+};
