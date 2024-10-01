530735e0d3360e5c5d64b6b514019c74efdc182f
Mika Andrianarijaona <mika.andrianarijaona@busbud.com>
2024-10-01 09:49:20 +0200

    feat: add backstage function

---
+++ dot_config/fish/functions/private_backstage.fish
@@ -0,0 +1,3 @@
+function backstage --wraps='zellij -l ~/Workspace/layouts/backstage.kdl' --description 'alias backstage=zellij -l ~/Workspace/layouts/backstage.kdl'
+    zellij -l ~/Workspace/layouts/backstage.kdl $argv
+end
