6d1d97ef770e72d467e2df495548cdf45e65b988
Mika Andrianarijaona <mika.andrianarijaona@busbud.com>
2024-11-08 15:05:17 +0100

    fix: put back buffer shortcut

--- dot_config/nvim/lua/plugins/window.lua
+++ dot_config/nvim/lua/plugins/window.lua
@@ -44,6 +44,71 @@ return {
 	{
 		"akinsho/bufferline.nvim",
 		event = "VeryLazy",
+		keys = {
+			{
+				"<leader>1",
+				function()
+					require("bufferline").go_to(1, true)
+				end,
+				desc = "Jump to buffer 1",
+			},
+			{
+				"<leader>2",
+				function()
+					require("bufferline").go_to(2, true)
+				end,
+				desc = "Jump to buffer 2",
+			},
+			{
+				"<leader>3",
+				function()
+					require("bufferline").go_to(3, true)
+				end,
+				desc = "Jump to buffer 3",
+			},
+			{
+				"<leader>4",
+				function()
+					require("bufferline").go_to(4, true)
+				end,
+				desc = "Jump to buffer 4",
+			},
+			{
+				"<leader>5",
+				function()
+					require("bufferline").go_to(5, true)
+				end,
+				desc = "Jump to buffer 5",
+			},
+			{
+				"<leader>6",
+				function()
+					require("bufferline").go_to(6, true)
+				end,
+				desc = "Jump to buffer 6",
+			},
+			{
+				"<leader>7",
+				function()
+					require("bufferline").go_to(7, true)
+				end,
+				desc = "Jump to buffer 7",
+			},
+			{
+				"<leader>8",
+				function()
+					require("bufferline").go_to(8, true)
+				end,
+				desc = "Jump to buffer 8",
+			},
+			{
+				"<leader>9",
+				function()
+					require("bufferline").go_to(9, true)
+				end,
+				desc = "Jump to buffer 9",
+			},
+		},
 		opts = {
 			options = {
 				diagnostics = "nvim_lsp",
