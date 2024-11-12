9b52331c81d8ca8355ca4a10fbf1684b541ced20
Mika Andrianarijaona <mika.andrianarijaona@busbud.com>
2024-11-12 09:40:26 +0100

    fix: lazyvim copilot import

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -19,7 +19,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.linting.eslint" },
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.dap.core" },
-		{ import = "lazyvim.plugins.extras.coding.copilot" },
+		{ import = "lazyvim.plugins.extras.ai.copilot" },
 		{ import = "plugins" },
 	},
 	defaults = {
