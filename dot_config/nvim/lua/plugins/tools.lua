62f42dd4a18990a559ef277e28931efe665cae4b
Mika Andrianarijaona <mika.andrianarijaona@busbud.com>
2025-01-16 16:39:52 +0100

    fix: add chezmoi fzf integration

--- dot_config/nvim/lua/plugins/tools.lua
+++ dot_config/nvim/lua/plugins/tools.lua
@@ -6,6 +6,21 @@ return {
 			require("chezmoi").setup({
 				-- your configurations
 			})
+
+			fzf_chezmoi = function()
+				require("fzf-lua").fzf_exec(require("chezmoi.commands").list(), {
+					actions = {
+						["default"] = function(selected, opts)
+							require("chezmoi.commands").edit({
+								targets = { "~/" .. selected[1] },
+								args = { "--watch" },
+							})
+						end,
+					},
+				})
+			end
+
+			vim.api.nvim_command("command! ChezmoiFzf lua fzf_chezmoi()")
 		end,
 	},
 }
