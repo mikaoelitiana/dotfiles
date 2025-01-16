109824c01275634a881d1848cde703cad610f818
Mika Andrianarijaona <mika.andrianarijaona@busbud.com>
2025-01-16 16:49:26 +0100

    fix: auto apply chezmoi file edit

--- dot_config/nvim/lua/plugins/tools.lua
+++ dot_config/nvim/lua/plugins/tools.lua
@@ -21,6 +21,19 @@ return {
 			end
 
 			vim.api.nvim_command("command! ChezmoiFzf lua fzf_chezmoi()")
+
+			--  Treat all files in chezmoi source directory as chezmoi files
+			--  https://github.com/xvzc/chezmoi.nvim?tab=readme-ov-file#treat-all-files-in-chezmoi-source-directory-as-chezmoi-files
+			vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+				pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
+				callback = function(ev)
+					local bufnr = ev.buf
+					local edit_watch = function()
+						require("chezmoi.commands.__edit").watch(bufnr)
+					end
+					vim.schedule(edit_watch)
+				end,
+			})
 		end,
 	},
 }
