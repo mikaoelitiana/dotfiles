7ba29c801df62b23d45857d4edcabc41a7a11d1b
Mika Andrianarijaona <mika.andrianarijaona@busbud.com>
2025-01-24 14:49:16 +0100

    fix: load overseer

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -1,16 +1,16 @@
 return {
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = function(_, opts)
-      table.insert(opts.routes, {
-        filter = {
-          event = "lsp",
-          kind = "message",
-          find = "Inlay Hints request failed. Requires TypeScript 4.4+.",
-        },
-        opts = { skip = true },
-      })
-    end,
-  },
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = function(_, opts)
+			table.insert(opts.routes, {
+				filter = {
+					event = "lsp",
+					kind = "message",
+					find = "Inlay Hints request failed. Requires TypeScript 4.4+.",
+				},
+				opts = { skip = true },
+			})
+		end,
+	},
 }
