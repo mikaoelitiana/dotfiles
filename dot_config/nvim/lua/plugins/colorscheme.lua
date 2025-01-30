166c90e5c695e10c742edb3d30fdf61ea63c66ac
Mika Andrianarijaona <mika.andrianarijaona@busbud.com>
2025-01-30 14:18:57 +0100

    fix: remove catppuccin colorscheme

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,12 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000, lazy = false },
-  {
-    "LazyVim/LazyVim",
-    opts = { colorscheme = "catppuccin-frappe" },
-  },
-  {
-    "luukvbaal/statuscol.nvim",
-    config = function()
-      require("statuscol").setup({})
-    end,
-  },
+	{
+		"LazyVim/LazyVim",
+		opts = { colorscheme = "catppuccin-frappe" },
+	},
+	{
+		"luukvbaal/statuscol.nvim",
+		config = function()
+			require("statuscol").setup({})
+		end,
+	},
 }
