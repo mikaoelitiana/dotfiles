61a2a56639c7eff51463d25015760337435d0682
Mika Andrianarijaona <mikaoelitiana@gmail.com>
2025-03-06 20:37:43 +0100

    fix: add auto-dark-mode

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -4,6 +4,9 @@ return {
 		opts = { colorscheme = "catppuccin-latte" },
 	},
 	{
+		"f-person/auto-dark-mode.nvim",
+	},
+	{
 		"luukvbaal/statuscol.nvim",
 		config = function()
 			require("statuscol").setup({})
