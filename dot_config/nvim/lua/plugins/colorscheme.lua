6f643752ed2279b45aeb736d5618e67720faf8cb
Mika Andrianarijaona <mikaoelitiana@gmail.com>
2025-03-06 20:48:43 +0100

    fix: colorscheme auto detect

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,18 @@
 return {
 	{
-		"LazyVim/LazyVim",
-		opts = { colorscheme = "catppuccin-latte" },
-	},
-	{
-		"f-person/auto-dark-mode.nvim",
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "latte",
+				background = {
+					light = "latte",
+					dark = "frappe",
+				},
+			})
+			vim.cmd.colorscheme("catppuccin")
+		end,
 	},
 	{
 		"luukvbaal/statuscol.nvim",
