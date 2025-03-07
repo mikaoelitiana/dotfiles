5973ac95a819dd3bd1e660dd27d883d8cc71075d
Mika Andrianarijaona <mika.andrianarijaona@busbud.com>
2025-03-07 12:01:46 +0100

    fix: lazyvim colorscheme

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -7,18 +7,13 @@ return {
 	},
 	{
 		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				flavour = "latte",
-				background = {
-					light = "latte",
-					dark = "frappe",
-				},
-			})
-			vim.cmd.colorscheme("catppuccin")
-		end,
+		lazy = true,
+		opts = {
+			background = {
+				light = "latte",
+				dark = "frappe",
+			},
+		},
 	},
 	{
 		"luukvbaal/statuscol.nvim",
