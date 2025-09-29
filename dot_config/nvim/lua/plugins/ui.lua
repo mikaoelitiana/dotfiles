1b7e87eca70e1c2026215121241a3237de3ea447
Mika Andrianarijaona <mika.andrianarijaona@busbud.com>
2025-09-29 14:59:48 +0200

    fix: remove temporary catpuccing fix

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -13,16 +13,4 @@ return {
 			})
 		end,
 	},
-	-- temporary fix for catppuccin bufferline integration
-	-- https://github.com/LazyVim/LazyVim/issues/6355#issuecomment-3212986215
-	{
-		"catppuccin/nvim",
-		opts = function(_, opts)
-			local module = require("catppuccin.groups.integrations.bufferline")
-			if module then
-				module.get = module.get_theme
-			end
-			return opts
-		end,
-	},
 }
