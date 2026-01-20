c8102a88eefb8e9abedbcc3696e546e1b3268dc6
Mika Andrianarijaona <mika.andrianarijaona@busbud.com>
2026-01-20 11:47:59 +0100

    fix: add config to avoid biome/prettier conflict

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -4,3 +4,6 @@
 
 -- load .nvimrc files in projects root
 vim.o.exrc = true
+-- https://www.lazyvim.org/extras/formatting/biome#options
+-- Enable this option to avoid Biome conflicts with Prettier.
+vim.g.lazyvim_prettier_needs_config = true
