42e8368f133fb4f18a1503dbb599c32941976821
Mika Andrianarijaona <mika.andrianarijaona@symbioticsec.ai>
2026-05-19 15:31:15 +0200

    add yannvanhalewyn/jujutsu.nvim

--- dot_config/nvim/lua/plugins/jj.lua
+++ dot_config/nvim/lua/plugins/jj.lua
@@ -1,9 +1,19 @@
 return {
-  {
-    "swaits/lazyjj.nvim",
-    dependencies = "nvim-lua/plenary.nvim",
-    opts = {
-      mapping = "<leader>jj",
-    },
-  },
+	{
+		"swaits/lazyjj.nvim",
+		dependencies = "nvim-lua/plenary.nvim",
+		opts = {
+			mapping = "<leader>jj",
+		},
+	},
+	{
+		"yannvanhalewyn/jujutsu.nvim",
+		dependencies = "sindrets/diffview.nvim",
+		opts = {
+			diff_preset = "diffview",
+		},
+		keys = {
+			{ "<leader>gj", function() require("jujutsu-nvim").log() end, desc = "Jujutsu log" },
+		},
+	},
 }
