d1670d6f88b0dac4f8df46de94143e194598c811
Mika Andrianarijaona <mika.andrianarijaona@symbioticsec.ai>
2026-05-26 12:10:15 +0200

    chore: clean unused files

--- dot_config/fish/conf.d/nix.fish
+++
@@ -1,5 +0,0 @@
-# Nix
-if test -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
-    . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
-end
-# End Nix
--- dot_config/fish/functions/private_backstage.fish
+++
@@ -1,3 +0,0 @@
-function backstage --wraps='zellij -l ~/Workspace/layouts/backstage.kdl' --description 'alias backstage=zellij -l ~/Workspace/layouts/backstage.kdl'
-    zellij -l ~/Workspace/layouts/backstage.kdl $argv
-end
