b318b091ad3a6552be84b6a82eb54e40120ccaef
Mika Andrianarijaona <mika.andrianarijaona@symbioticsec.ai>
2026-05-28 14:43:17 +0200

    fix(ai): remove sidekick.nvim config

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -6,33 +6,6 @@ wk.add({
 
 return {
 	{
-		"folke/sidekick.nvim",
-		opts = {
-			cli = {
-				mux = {
-					enabled = true,
-					backend = "zellij",
-				},
-				win = {
-					keys = {
-						prompt = { "<c-y>", "prompt", mode = "t", desc = "insert prompt or context" },
-					},
-				},
-				tools = {
-					symbiotic = {
-						cmd = { "symbiotic" },
-						continue = { "--continue" },
-						native_scroll = true,
-						env = {
-							-- HACK: https://github.com/sst/opencode/issues/445
-							OPENCODE_THEME = "system",
-						},
-					},
-				},
-			},
-		},
-	},
-	{
 		"carlos-algms/agentic.nvim",
 		opts = {
 			provider = "symbiotic",
